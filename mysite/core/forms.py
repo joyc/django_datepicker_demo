9522cd5cc9f5c65de6bc880077b93a5166338b15
joyc <joyc@live.jp>
2020-12-30 19:57:38 +0900

    init commit

---
+++ mysite/core/forms.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File   : forms.py
+# @Author : Hython
+# @Date   : 公元 2020/12/30 15:20
---
+++ mysite/core/widgets.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File   : widgets.py
+# @Author : Hython
+# @Date   : 公元 2020/12/30 15:21
